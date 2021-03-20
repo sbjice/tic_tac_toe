8a61ca7342acc9087e7048ee0d6b50ca2d3ab1bb
sbjice <boy-ram@mail.ru>
2021-03-20 17:16:30 +0300

    Create main loop with game continue Create automatic change of player Move to classes Update tests to OOP

--- main.py
+++ main.py
@@ -1,93 +1,148 @@
-def print_game_field(field):
-    for row in field:
-        [print(row[i], end=' | ') if i < 2 else print(row[i]) for i in range(len(row))]
-        print(f'---------')
+class TicTacToe:
+    # counter = 0
 
+    def __init__(self):
+        # TicTacToe.counter += 1
+        # if TicTacToe.counter > 1:
+        #     return None
+        self.field = []
+        self.current_choice = ' '
+        self.field = self.get_empty_field()
+        self.main_loop()
 
-def get_empty_field():
-    return [[' ' for __ in range(3)] for _ in range(3)]
+    def print_game_field(self):
+        for row in self.field:
+            [print(row[i], end=' | ') if i < 2 else print(row[i]) for i in range(len(row))]
+            print(f'---------')
 
+    @staticmethod
+    def get_empty_field():
+        return [[' ' for __ in range(3)] for _ in range(3)]
 
-def get_coordinates():
-    x = 0
-    while x not in range(1, 4):
-        x = int(input("Enter row number (from 1 to 3)"))
-    y = 0
-    while y not in range(1, 4):
-        y = int(input("Enter column number (from 1 to 3)"))
-    x -= 1
-    y -= 1
+    @staticmethod
+    def get_full_field():
+        return [['x' for __ in range(3)] for _ in range(3)]
 
-    print(f"row: {x}, column: {y}")
-    return x, y
+    @staticmethod
+    def get_coordinates():
+        x = 0
+        while x not in range(1, 4):
+            try:
+                x = int(input("Enter row number (from 1 to 3): "))
+            except Exception as e:
+                print(e)
+        y = 0
+        while y not in range(1, 4):
+            try:
+                y = int(input("Enter column number (from 1 to 3): "))
+            except Exception as e:
+                print(e)
+        x -= 1
+        y -= 1
 
+        print(f"row: {x}, column: {y}")
+        return x, y
 
-def point_can_be_filled(field, row, col):
-    return field[row][col] == ' '
+    def point_can_be_filled(self, row, col):
+        return self.field[row][col] == ' '
 
+    def column_is_filled_with_given_choice(self, col=0, choice='x'):
+        for row in self.field:
+            if row[col] != choice:
+                return False
+        return True
 
-def column_is_filled_with_given_choice(field, col=0, choice='x'):
-    for row in field:
-        if row[col] != choice:
-            return False
-    return True
+    def row_is_filled_with_given_choice(self, row=0, choice='x'):
+        for col in range(len(self.field)):
+            if self.field[row][col] != choice:
+                return False
+        return True
 
+    def winning_on_rows_with_given_choice(self, choice='x'):
+        for i in range(len(self.field)):
+            if self.row_is_filled_with_given_choice(i, choice):
+                return True
+        return False
 
-def row_is_filled_with_given_choice(field, row=0, choice='x'):
-    for col in range(len(field)):
-        if field[row][col] != choice:
-            return False
-    return True
+    def winning_on_columns_with_given_choice(self, choice='x'):
+        for i in range(len(self.field)):
+            if self.column_is_filled_with_given_choice(i, choice):
+                return True
+        return False
 
+    def winning_on_main_diagonal_with_given_choice(self, choice='x'):
+        for i in range(len(self.field)):
+            if self.field[i][i] != choice:
+                return False
+        return True
 
-# row_val, col_val = get_coordinates()
-# while not point_can_be_filled(test_field, row_val, col_val):
-#     print('The given point is not free, enter new point!')
-#     row_val, col_val = get_coordinates()
+    def winning_on_side_diagonal_with_given_choice(self, choice='x'):
+        for i in range(len(self.field)):
+            if self.field[len(self.field)-1-i][i] != choice:
+                return False
+        return True
 
-
-def winning_on_rows_with_given_choice(field, choice='x'):
-    for i in range(len(field)):
-        if row_is_filled_with_given_choice(field, i, choice):
+    def winning_for_given_choice(self, choice='x'):
+        if self.winning_on_main_diagonal_with_given_choice(choice):
             return True
-    return False
-
-
-def winning_on_columns_with_given_choice(field, choice='x'):
-    for i in range(len(field)):
-        if column_is_filled_with_given_choice(field, i, choice):
+        if self.winning_on_side_diagonal_with_given_choice(choice):
             return True
-    return False
+        if self.winning_on_columns_with_given_choice(choice):
+            return True
+        if self.winning_on_rows_with_given_choice(choice):
+            return True
+        return False
 
-
-def winning_on_main_diagonal_with_given_choice(field, choice='x'):
-    for i in range(len(field)):
-        if field[i][i] != choice:
-            return False
-    return True
-
-
-def winning_on_side_diagonal_with_given_choice(field, choice='x'):
-    for i in range(len(field)):
-        if field[len(field)-1-i][i] != choice:
-            return False
-    return True
-
-
-def winning_for_given_choice(field, choice='x'):
-    if winning_on_main_diagonal_with_given_choice(field, choice):
+    def field_filled(self):
+        for row in self.field:
+            for val in row:
+                if val == ' ':
+                    return False
         return True
-    if winning_on_side_diagonal_with_given_choice(field, choice):
+
+    @staticmethod
+    def field_given_filled(field):
+        for row in field:
+            for val in row:
+                if val == ' ':
+                    return False
         return True
-    if winning_on_columns_with_given_choice(field, choice):
-        return True
-    if winning_on_rows_with_given_choice(field, choice):
-        return True
-    return False
+
+    def fill_field_until_win_or_draw(self):
+        self.current_choice = 'x'
+        while True:
+            print(f"Player with {self.current_choice} should put his figure: ")
+            row_val, col_val = self.get_coordinates()
+            while not self.point_can_be_filled(row_val, col_val):
+                print('The given point is not free, enter new point!')
+                row_val, col_val = self.get_coordinates()
+            self.field[row_val][col_val] = self.current_choice
+            self.print_game_field()
+
+            if self.winning_for_given_choice(self.current_choice):
+                print(f"The player with {self.current_choice} wins!")
+                return
+            if self.field_filled():
+                print("The Draw!")
+                return
+            if self.current_choice == 'x':
+                self.current_choice = 'o'
+            else:
+                self.current_choice = 'x'
+
+    def main_loop(self):
+        keep_playing = None
+        while keep_playing != 'n':
+            keep_playing = input("Do you want to play TicTacToe? (y or n): ")
+            if keep_playing == 'y':
+                self.fill_field_until_win_or_draw()
+
+
+# TODO: Create AI for playing with PC
+
+
+if __name__ == "__main__":
+    t1 = TicTacToe()
 
 
 
-# TODO 1: Move to classes
-# TODO 2: Create field autofill consistent with current player change
-# TODO 3: Create AI for playing with PC
-# TODO 4: Create main loop with game reset
--- tests.py
+++ tests.py
@@ -1,51 +1,63 @@
-from main import *
+from main import TicTacToe
 
-test_field = get_empty_field()
-test_field[2][0] = 'x'
-test_field[2][1] = '0'
-test_field[2][2] = 'x'
-print_game_field(test_field)
+t = TicTacToe()
+t.field[2][0] = 'x'
+t.field[2][1] = 'o'
+t.field[2][2] = 'x'
+t.print_game_field()
 
-print(winning_on_rows_with_given_choice(test_field, 'x'))
 print('winning on rows')
+print(t.winning_on_rows_with_given_choice('x'))
 
-test_field = get_empty_field()
-test_field[0][2] = 'x'
-test_field[1][2] = '0'
-test_field[2][2] = 'x'
-print_game_field(test_field)
+t = TicTacToe()
+t.field[0][2] = '0'
+t.field[1][2] = '0'
+t.field[2][2] = '0'
+t.print_game_field()
 
-print(winning_on_columns_with_given_choice(test_field, 'x'))
 print('winning on cols')
+print(t.winning_on_columns_with_given_choice('x'))
 
+t = TicTacToe()
+t.field[0][0] = 'x'
+t.field[1][1] = 'x'
+t.field[2][2] = 'x'
+t.print_game_field()
 
-test_field = get_empty_field()
-test_field[0][0] = 'x'
-test_field[1][1] = 'x'
-test_field[2][2] = 'x'
-print_game_field(test_field)
-
-print(winning_on_main_diagonal_with_given_choice(test_field, 'x'))
+print(t.winning_on_main_diagonal_with_given_choice('x'))
 print('winning on main diagonal')
 
-test_field = get_empty_field()
-test_field[0][2] = 'x'
-test_field[1][1] = ' '
-test_field[2][0] = 'x'
-print_game_field(test_field)
+t = TicTacToe()
+t.field[0][2] = 'x'
+t.field[1][1] = 'x'
+t.field[2][0] = 'x'
+t.print_game_field()
 
-print(winning_on_side_diagonal_with_given_choice(test_field, 'x'))
+print(t.winning_on_side_diagonal_with_given_choice('x'))
 print('winning on side diagonal')
 
 
-test_field = get_empty_field()
-test_field[0][2] = 'x'
-test_field[1][1] = 'x'
-test_field[2][0] = 'x'
-test_field[0][1] = 'o'
-test_field[1][0] = 'o'
-test_field[2][1] = 'o'
-test_field[1][2] = 'o'
-print_game_field(test_field)
-print(winning_for_given_choice(test_field, 'x'))
+t = TicTacToe()
+t.field[0][2] = 'x'
+t.field[1][1] = 'x'
+t.field[2][0] = 'x'
+t.field[0][1] = 'o'
+t.field[1][0] = 'o'
+t.field[2][1] = 'o'
+t.field[1][2] = 'o'
+t.print_game_field()
+print(t.winning_for_given_choice('x'))
 print('winning')
+
+full_field = t.get_full_field()
+empty_field = t.get_empty_field()
+
+print('check full_field is full')
+print(t.field_given_filled(full_field))
+print('check empty_field is full')
+print(t.field_given_filled(empty_field))
+empty_field = t.get_empty_field()
+
+# fill_field_until_win_or_draw(empty_field)
+
+
